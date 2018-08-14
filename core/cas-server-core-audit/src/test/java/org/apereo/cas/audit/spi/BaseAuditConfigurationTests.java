0e6c9d09090490b0be1175374c1760022f9dc608
Timur Duehr <tduehr@gmail.com>
2018-08-13 22:18:46 -0500

    base tests

---
+++ core/cas-server-core-audit/src/test/java/org/apereo/cas/audit/spi/BaseAuditConfigurationTests.java
@@ -0,0 +1,48 @@
+package org.apereo.cas.audit.spi;
+
+import org.apereo.cas.util.DateTimeUtils;
+import org.apereo.cas.util.junit.ConditionalIgnoreRule;
+
+import lombok.val;
+import org.apereo.inspektr.audit.AuditActionContext;
+import org.apereo.inspektr.audit.AuditTrailManager;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link BaseAuditConfigurationTests}.
+ *
+ * @author Timur Duehr
+ * @since 6.0.0
+ */
+public abstract class BaseAuditConfigurationTests {
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+    @Rule
+    public final ConditionalIgnoreRule conditionalIgnoreRule = new ConditionalIgnoreRule();
+
+    public abstract AuditTrailManager getAuditTrailManager();
+
+    @Test
+    public void verifyAuditManager() {
+        val time = LocalDate.now().minusDays(2);
+        val since = DateTimeUtils.dateOf(time);
+        val ctx = new AuditActionContext("casuser", "TEST", "TEST",
+            "CAS", since, "1.2.3.4",
+            "1.2.3.4");
+        getAuditTrailManager().record(ctx);
+        val results = getAuditTrailManager().getAuditRecordsSince(time);
+        assertFalse(results.isEmpty());
+    }
+}
